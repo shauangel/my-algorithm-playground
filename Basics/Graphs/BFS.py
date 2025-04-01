2608a95dc3742bc714e66cef201e8fe31be781fb
angelshau <shauangel2000@gmail.com>
2025-04-01 15:21:26 -0400

    update basics

---
+++ Basics/Graphs/BFS.py
@@ -0,0 +1,17 @@
+"""
+Breadth First Search
+"""
+import json
+
+class BFS:
+    def __init__(self, G):
+        self.G = G
+
+    def compute(self):
+        return 
+
+
+if __name__ == "__main__":
+    with open("../data/g1.json", 'r') as f:
+        data = json.load(f)
+    print(data)
---
+++ Basics/Greedy/Matroid.py
@@ -0,0 +1,4 @@
+"""
+A more general 
+
+"""
