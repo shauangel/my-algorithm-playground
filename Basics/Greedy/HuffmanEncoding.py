e155261b1e3a7e8b19b66a8c4302c52eb4fac0be
angelshau <shauangel2000@gmail.com>
2025-04-01 09:55:54 -0400

    update greedy

---
+++ Basics/Greedy/HuffmanEncoding.py
@@ -0,0 +1,10 @@
+"""
+Huffman Encoding
+-> a method to compress strings into bits
+* binary tree
+* O(nlogn) using priority queue
+"""
+
+class Huffman:
+    def __init__(self):
+        return
---
+++ Basics/Greedy/Prim-MST.py
@@ -0,0 +1,11 @@
+"""
+Variables:
+S: a set to record current included tree
+cost(v): min weight
+prev(v): keep tracks of tree
+
+"""
+
+class PrimMST:
+    def __init__(self, G):
+        return
